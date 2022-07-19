6b358fbab2f9e38ad82b415263d4bd6117aa4263
Bruna Sobral <107651452+sobruna@users.noreply.github.com>
2022-07-19 18:01:52 -0300

    Update + fix
    Novas páginas inclusas;
    Footer funcionando em todas as páginas;
    MVP das paginas: cursos, contato, cadastro.
    
    To fix: Resposividade dos ícones de mídias sociais do footer e da páginas "Contato"

---
+++ javascript/trabalheconosco.js
@@ -0,0 +1,38 @@
+let vagas = [{
+    titulo: "Analista de CX",
+    local: "São Paulo/ Remoto",
+    area: "Operações",
+    nivel: "Pleno/ Sênior"
+    },
+    {
+    titulo: "Analista de Suporte",
+    local: "Remoto",
+    area: "Operações",
+    nivel: "Todos"   
+    },
+    {
+    titulo: "AML/ KYC",
+    local: "São Paulo/ Remoto",
+    area: "Compliance",
+    nivel: "Sênior"   
+    },
+    {
+    titulo: "Software Engineer",
+    local: "São Paulo/ Remoto",
+    area: "Engenharia & Produto",
+    nivel: "Todos"  
+    },
+    {
+    titulo: "Data Scientist",
+    local: "São Paulo/ Remoto",
+    area: "Engenharia & Produto",
+    nivel: "Pleno/ Sênior"  
+    },
+    {
+    titulo: "Mobile Developer",
+    local: "São Paulo/ Remoto",
+    area: "Engenharia & Produto",
+    nivel: "Todos"  
+    }
+]
+
