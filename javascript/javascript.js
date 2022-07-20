bc094f156113b96743f8ddff956e38a924392f36
andrezaaraujo <andreza.si.araujo@gmail.com>
2022-07-19 22:04:55 -0400

    Alterações Login

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -22,3 +22,8 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+function alerta(){
+alert("Nenhum cadastro realizado!")
+}
