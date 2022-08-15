ccce3e088e7c9098531f1001a47e50cf9806502b
andrezaaraujo <andreza.si.araujo@gmail.com>
2022-08-14 23:21:35 -0400

    User logado

--- javascript/login.js
+++ javascript/login.js
@@ -1,15 +1,27 @@
+
 function login(){
     let email = document.getElementById("email")
     let senha = document.getElementById("senha")
+    
 
 
-    if((email.value == "andreza@admin" && senha.value== "admin") ||(email.value == "bruna@admin" && senha.value== "admin") ){
-        window.location.href = "index.html";
+    if((email.value == "andreza" && senha.value== "admin") ||(email.value == "bruna" && senha.value== "admin") ){
+        window.location.href = "logado.html";
+        
+        
     }
     else{
         alert("Senha ou login inválidos!")
     }
+      
+    console.log("teste");
+
 }
+
+console.log("teste");
+let email = document.getElementById("email")
+let html =  document.getElementById("logado").innerHTML;
+document.getElementById("logado").innerHTML = html;
 
 let input = document.getElementById("senha");
     input.addEventListener("keyup", function(event) {
@@ -18,3 +30,8 @@ let input = document.getElementById("senha");
              document.getElementById("teste").click();
         }
     });
+
+
+
+
+        
