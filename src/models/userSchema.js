f1186b98d966504e7bdeabd0f722345edb223974
andrezaaraujo <andreza.si.araujo@gmail.com>
2022-09-14 21:55:25 -0400

    Update userSchema.js

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -10,6 +10,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    phone: {
+        type: String,required: true
+    },
     password: {
         type: String,
         required: false
