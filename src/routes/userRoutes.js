a0b4a5822b17cbb40a064e239575a8f21bd0593a
andrezaaraujo <andreza.si.araujo@gmail.com>
2022-09-14 21:54:19 -0400

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,9 @@ const controller = require("../controllers/userController")
 
 router.get("/all", controller.getAll)
 router.post("/create", controller.createUser)
+//No postman testar rota com ID do usiário que deseja atualizar
+router.patch("/update/:id", controller.updateUser);
+//Delete
+router.delete("/delete/:id", controller.deleteUser);
 
 module.exports = router
